a6408408cfebfab0c6f4058ca37064eb724c859d
Amir Hossein saffarnia <dev.saffarnia@gmail.com>
2025-06-08 15:46:00 +0330

    modification on main.ts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './app.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import i18n from './i18n'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(i18n)
+
+app.mount('#app')
